2c2fbac13e2669f8668ee5c2db152ea0493cea0b
Eric <eric.yanez50@qmail.cuny.edu>
2021-10-16 12:12:35 -0400

    done

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const farmAnimals = 'cow horse sheep pig chicken';
+ let [moo, neigh, baa, oink, cluck] = farmAnimals.split(' ');
+ let [bessie, , dolly, babe, little] = farmAnimals.split(' ');
+ let [blackAndWhite, , black, pink, ,] =farmAnimals.split(' ');
 
 const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const [red, orange, yellow, green, blue, indigo, violet] = colors;
+const [r,o,y,g,b,,v] =colors;
+const [, , , , ,indg,] =colors;
 
 const muppet = {
   muppetName: 'Miss Piggy',
@@ -9,6 +15,7 @@ const muppet = {
   job: 'Cast member of The Muppet Show',
   partner: 'Kermit'
 };
+const {muppetName, color, song, job, partner} = muppet;
 
 const nestedMuppet = {
   nestedName: 'Kermit',
@@ -24,14 +31,17 @@ const nestedMuppet = {
   nestedJob: 'Host of The Muppet Show',
   nestedPartner: 'Miss Piggy'
 };
+const{song2,song4} = nestedMuppet.album.theMuppetMovie
+const{nestedJob} = nestedMuppet;
+const{nestedPartner} = nestedMuppet
 
 // Strings
 
-// 1. Use destructuring to assign appropriate variables based on the sounds animals make.
+// 1. Use destructuring to assign appropriate variables based on the sounds animals make. DONE
 
-// 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.
+// 2. Bolt the horse wandered off, so just give us four animals, and let's name them bessie, dolly, babe, and little.DONE
 
-// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.
+// 3. Little the chicken had to go back to the coop, so now we're left with three. Let's use color variables of blackAndWhite, black, and pink.DONE
 
 // Arrays
 
